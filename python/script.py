eaf86947934d022ad86f468699e367a17c0295bd
bisseck <simonbisseck2@gmail.com>
2024-01-30 02:59:02 +0000

    cool

--- python/script.py
+++ python/script.py
@@ -1,15 +1,4 @@
 # coding:utf-8
-# premier programme sur python
-# afficher du texte a l'ecran
-print("premier programme sur python")
-# aller a la ligne pendant la saisie
-print("premier programme sur python\n super")
-""" 
-les trois corte permettent
-de faire les commantaires su 
-plusieurs lignes
-"""
-"""
-pour afficher du texte sur l'interpreteur se placer
-avec le power shell dans le dossier e travail et taper 
-python script.py """ 
+# les vaariables
+AgePersonne = 14 
+print(type(AgePersonne))
